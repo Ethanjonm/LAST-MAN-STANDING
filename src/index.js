773e503cba00d642526a100e68d815186a02f908
Ethanjonm <ethanjonmercado@gmail.com>
2023-05-10 03:15:57 -0400

    deleted unused files

--- src/index.js
+++ src/index.js
@@ -1,13 +1,3 @@
-// import Example from './scripts/example';
-
-// document.addEventListener("DOMContentLoaded", () => {
-//     console.log("Hello World");
-
-//     const main = document.getElementById('main');
-//     new Example(main)
-
-// })
-// //npm run watch
 
 
 import Player from "./scripts/player"
@@ -167,7 +157,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }
     
     animate();
-    // spawnZombie();
+    spawnZombie();
 
 })  
 
